34c46b97653e554c58b70cb68f56eeeafd3a899d
yusukeagata <yusuke.agata1013@gmail.com>
2016-06-26 10:01:09 +0000

    0626-1901

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update,:following, :followers]
+  before_action :logged_in_user, only: [:edit, :update, :following, :followers]
+  before_action :find
 
   def show # 追加
-   @user = User.find(params[:id])
    @microposts = @user.microposts.order(created_at: :desc)
   end
   
@@ -19,20 +19,8 @@ class UsersController < ApplicationController
         render 'new'
       end
   end
-#def current_user
-#    if (user_id = session[:user_id])
-#      @current_user ||= User.find_by(id: user_id)
-#    elsif (user_id = cookies.signed[:user_id])
-#      user = User.find_by(id: user_id)
-#      if user && user.authenticated?(cookies[:remember_token])
-#        log_in user
-#        @current_user = user
-#      end
-#    end
-#end
-
+  
   def edit
-    @user = User.find(params[:id]) #編集しようとしたユーザー
     if (current_user != @user)
       redirect_to root_url
     end
@@ -41,7 +29,6 @@ class UsersController < ApplicationController
   end
   
   def update
-    @user = User.find(params[:id]) #編集しようとしたユーザー
     if (current_user != @user)
       redirect_to root_url
     end
@@ -49,7 +36,7 @@ class UsersController < ApplicationController
      #edit 画面のボタンが押されたらやってくる
     if @user.update(user_params2)
       # 保存に成功した場合はトップページへリダイレクト
-      redirect_to root_path , notice: 'メッセージを編集しました'
+      redirect_to root_path , notice: 'プロフィールを編集しました'
     else
       # 保存に失敗した場合は編集画面へ戻す
       render 'edit'
@@ -57,27 +44,29 @@ class UsersController < ApplicationController
   end
   def following
     @title = "Following"
-    @user  = User.find(params[:id])
     @users = @user.following_users.order(created_at: :desc)
     render 'show_follow'
   end
 
   def followers
     @title = "Followers"
-    @user  = User.find(params[:id])
     @users = @user.follower_users.order(created_at: :desc)
     render 'show_follow'
   end
 
   private
+  
+  def find
+    @user  = User.find(params[:id])
+  end
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
   end
  
-   def user_params2
+  def user_params2
     params.require(:user).permit(:name, :email, :region, :password,
                                  :password_confirmation, )
-   end 
+  end 
   
 end
